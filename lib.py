b6b92b929e77bcbd3bc4bfe408fca0a378c3578a
lepisma <abhinav.tushar.vs@gmail.com>
2014-03-12 18:59:38 +0530

    removes array support from getPositions function;

--- lib.py
+++ lib.py
@@ -19,56 +19,47 @@ def clearNoise(img):
 
 #Filter Out Each ColorBand covered Finger
 
-def filterFingers(img):
+def filterBlob(img):
 		
 	height = img.shape[0]
 	width = img.shape[1]
 
-	imgcropped = img[0:(height/2), (width/2):(width-1)] # Cropping the ROI
-
-	fingerArray = []
+	blobArray = []
 
 	min = np.array([ranges[0][0][0], ranges[0][1][0], ranges[0][2][0]], np.uint8)
 	max = np.array([ranges[0][0][1], ranges[0][1][1], ranges[0][2][1]], np.uint8)
 	red = cv2.inRange(imgcropped, min, max)
 	red = clearNoise(red)
-	fingerArray.append(red)
+	blobArray.append(red)
 
 	min = np.array([ranges[1][0][0], ranges[1][1][0], ranges[1][2][0]], np.uint8)
 	max = np.array([ranges[1][0][1], ranges[1][1][1], ranges[1][2][1]], np.uint8)
 	green = cv2.inRange(imgcropped, min, max)
 	green = clearNoise(green)
-	fingerArray.append(green)
+	blobArray.append(green)
 
 	min = np.array([ranges[2][0][0], ranges[2][1][0], ranges[2][2][0]], np.uint8)
 	max = np.array([ranges[2][0][1], ranges[2][1][1], ranges[2][2][1]], np.uint8)
 	blue = cv2.inRange(imgcropped, min, max)
 	blue = clearNoise(blue)
-	fingerArray.append(blue)
+	blobArray.append(blue)
 
-	# Returns an array of three filtered fingers images
-	return fingerArray
+	# Returns an array of three colored blobs
+	return blobArray
 
-#Get coordinates of each fingers/bands topmost point
-
-def getPositions(imgArray):
+def getPositions(img):
 	# Returns the topmost points of filtered blobs from given imgs
 
 	positions=[]
 
-	for img in imgArray:
+	contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-		contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	if len(contours)<1:
+		positions.append((-1, -1))
+		continue
 
-		if len(contours)<1:
-			positions.append((0,0))
-			continue
-
-
-		cnt=contours[0]
-
-		topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
-
-		positions.append(topmost)
+	cnt=contours[0]
+	topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
+	positions.append(topmost)
 
 	return positions
